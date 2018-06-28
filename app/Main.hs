e3c8f98df1e6846b815be5d7416b1dcc6a889363
Gabriel Couture <gab-couture@hotmail.com>
2018-06-28 14:39:03 -0400

    now Pakell creat a config file if it does not exist

--- app/Main.hs
+++ app/Main.hs
@@ -3,6 +3,7 @@ module Main where
 
 import Turtle
 ----------------
+import Utils   ( filePathToString )
 import Parsing ( parserMain
                , parserVersion
                , parserTODO
@@ -12,9 +13,32 @@ import Parsing ( parserMain
                , parserOPTIMIZE
                , parserBUG )
 
+-- TEMP : for testing
+-- TODO : test
 
 main :: IO ()
 main = do
+  -- create config file if not exist
+  -- ---------------------------------------------------------------------
+  -- FilePath type, home dir (/home/<user>)
+  homePath <- home
+
+  -- String of config path
+  let configPath   = encodeString homePath ++ "/.config"
+  let pakellConfig = encodeString homePath ++ "/.config/pakell.conf"
+
+  -- getting boolean : True if file exists; False if not exists
+  configPathExist <- testdir  $ fromString configPath
+  configExist     <- testfile $ fromString pakellConfig
+
+  -- if .config does not exist, create it
+  when (not configPathExist) $ mkdir $ fromString configPath
+
+  -- if pakell.conf does not exist, create it
+  when (not configExist) $ touch $ fromString pakellConfig
+  -- ---------------------------------------------------------------------
+
+  -- command
   cmd <- options "Options for pakell parsing tool" parser
   cmd
 
